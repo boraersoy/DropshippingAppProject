f0bd66b0ed421c6f9343ea6d267a538d8ff723ff
Bora Ersoy <95964606+boraersoy@users.noreply.github.com>
2022-10-20 22:34:07 +0300

    Add files via upload

--- DropShippinProject/src/Product.java
+++ DropShippinProject/src/Product.java
@@ -5,5 +5,49 @@ public class Product {
 	private double Rate;
 	private int Number_of_Reviews;
 	private int Price;
+	public Product(int iD,
+			String title,
+			double rate, int number_of_Reviews, int price) {
+		ID = iD;
+		Title = title;
+		Rate = rate;
+		Number_of_Reviews = number_of_Reviews;
+		Price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [ID=" + ID + ", Title=" + Title + ", Rate=" + Rate + ", Number_of_Reviews=" + Number_of_Reviews
+				+ ", Price=" + Price + "]";
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public double getRate() {
+		return Rate;
+	}
+	public void setRate(double rate) {
+		Rate = rate;
+	}
+	public int getNumber_of_Reviews() {
+		return Number_of_Reviews;
+	}
+	public void setNumber_of_Reviews(int number_of_Reviews) {
+		Number_of_Reviews = number_of_Reviews;
+	}
+	public int getPrice() {
+		return Price;
+	}
+	public void setPrice(int price) {
+		Price = price;
+	}
 	
 }
--- DropShippinProject/src/Sales.java
+++ DropShippinProject/src/Sales.java
@@ -3,6 +3,51 @@ public class Sales {
 	private int ID;
 	private String Customer;
 	private String Product;
+	@Override
+	public String toString() {
+		return "Sales [ID=" + ID + ", Customer=" + Customer + ", Product=" + Product + ", SalesData=" + SalesData
+				+ ", SalesPrice=" + SalesPrice + "]";
+	}
+	public Sales(int iD,
+			String customer,
+			String product, String salesData, int salesPrice) {
+		super();
+		ID = iD;
+		Customer = customer;
+		Product = product;
+		SalesData = salesData;
+		SalesPrice = salesPrice;
+	}
 	private String SalesData;
 	private int SalesPrice;
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getCustomer() {
+		return Customer;
+	}
+	public void setCustomer(String customer) {
+		Customer = customer;
+	}
+	public String getProduct() {
+		return Product;
+	}
+	public void setProduct(String product) {
+		Product = product;
+	}
+	public String getSalesData() {
+		return SalesData;
+	}
+	public void setSalesData(String salesData) {
+		SalesData = salesData;
+	}
+	public int getSalesPrice() {
+		return SalesPrice;
+	}
+	public void setSalesPrice(int salesPrice) {
+		SalesPrice = salesPrice;
+	}
 }
